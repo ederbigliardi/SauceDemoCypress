265869ef5aac6cd96adbd38b9a6f2faaee7819bd
Eder <edervb@gmail.com>
2024-12-15 20:42:52 -0300

    Coloca e retira do carrinho de Compras

--- cypress/e2e/selecionarProduto.cy.js
+++ cypress/e2e/selecionarProduto.cy.js
@@ -1,52 +1,88 @@
+import 'cypress-xpath'
+
 describe('Selecionar Produtos', () => {
   // atributos
 
   beforeEach(() => {
     cy.visit('/')       // abre o browser na url informada em cypress.config.js
-}) // termina before
+  }) // termina before
 
-it('Selecionar Sauce Labs Backpack', () => {
+  it('Selecionar Sauce Labs Backpack', () => {
 
-  cy.title()          // verifica se o título da página é Swag Labs
+    cy.title()          // verifica se o título da página é Swag Labs
       .should('eq', 'Swag Labs')
 
-  // realizar o login
-  cy.get('input[data-test="username"]') // preenche o usuário
+    // realizar o login
+    cy.get('input[data-test="username"]') // preenche o usuário
       .type('standard_user')
 
-  cy.get('#password')
+    cy.get('#password')
       .type('secret_sauce')             // preenche a senha
 
-  cy.get('input[name="login-button"]')
+    cy.get('input[name="login-button"]')
       .click()                          // clica no botão Login
 
-  // carregar a pagina de inventário  
+    // carregar a pagina de inventário  
 
-cy.get('span.title')
-  .should('have.text', 'Products')  // verifica se o elemento contém Products
+    cy.get('span.title')
+      .should('have.text', 'Products')  // verifica se o elemento contém Products
 
-cy.get('img[alt="Sauce Labs Backpack"]')
-  .click()  // clica na imagem do produto mochila
+    cy.get('img[alt="Sauce Labs Backpack"]')
+      .click()  // clica na imagem do produto mochila
 
-// carregar a pagina de item de inventário
+    // carregar a pagina de item de inventário
 
-// apenas para demonstrar como fariamos com Xpath Absoluto
-// verifica se no elemento via XPath contém o texto Back to products
-// Tem pelo menos 7 formas mais práticas e legiveis do que isso ...
-cy.xpath('/html/body/div/div/div/div[1]/div[2]/div/button')
-  .should('have.text', 'Back to products')
+    // apenas para demonstrar como fariamos com Xpath Absoluto
+    // verifica se no elemento via XPath contém o texto Back to products
+    // Tem pelo menos 7 formas mais práticas e legiveis do que isso ...
+    cy.xpath('/html/body/div[1]/div/div/div[1]/div[2]/div/button')
+      .should('have.text', 'Back to products')
 
-cy.get('div.inventory_details_name.large_size') // Verifica titulo
-  .should('have.text', 'Sauce Labs Backpack')
+    cy.get('div.inventory_details_name.large_size') // Verifica titulo
+      .should('have.text', 'Sauce Labs Backpack')
 
-cy.get('div.inventory_details_price') // Verifica preço
-  .should('have.text', '$29.99')
+    cy.get('div.inventory_details_price') // Verifica preço
+      .should('have.text', '$29.99')
 
-cy.get('#add-to-cart')
-  .click()  // clica no botão Adicionar no Carrinho
+    cy.get('#add-to-cart')
+      .click()  // clica no botão Adicionar no Carrinho
 
-cy.get('a.shopping_cart_link')
-  .should('have.text', '1') // verifica se no carrinho exibe o nº 1
+    cy.get('a.shopping_cart_link')
+      .should('have.text', '1') // verifica se no carrinho exibe o nº 1
+      .click()                  // clica para ir para o carrinho
 
-}) //termina o it
-})
+    cy.get('span.title')
+      .should('have.text', 'Your Cart')      // verifica o título da seção
+
+    cy.get('div.inventory_item_name')
+      .should('have.text', 'Sauce Labs Backpack')   // verifica o título do produto
+
+    cy.get('div.inventory_item_price')
+      .should('have.text', '$29.99')                // verifica o preço do produto
+
+    cy.get('div.cart_quantity')
+      .should('have.text', '1')                     // verifica a quantidade
+
+
+  }) //termina o it
+
+  afterEach(() => {
+    cy.get('#remove-sauce-labs-backpack')   // remove o produto do carrinho
+      .click()
+
+
+    cy.get('#react-burger-menu-btn')
+      .click()                            // clica no icone 3 traços
+
+
+    cy.get('#logout_sidebar_link', { timeout: 10000 })
+      .should('be.visible')         // esperar até que o elemento esteja visível
+      // .should('not.be.disabled')   // esperar até que ele esteja visível
+      .click()                             // clica na opção logout
+
+    cy.get('#login-button')
+      .should('be.visible') // verificar se está novamente na tela de login
+
+  }) // termina o afterEach
+
+}) // Termina describe
